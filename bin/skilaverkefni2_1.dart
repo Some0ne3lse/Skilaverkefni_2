eb6ab2b41ce3172af78775a5dce405116fe17474
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2023-10-05 17:03:06 +0000

    Fix typo

--- bin/skilaverkefni2_1.dart
+++ bin/skilaverkefni2_1.dart
@@ -1,7 +1,7 @@
 void main() {
   // Assignment 1
   print("This program writes the numbers 1-10 and adds the number to the total"
-      "of the previous numbers");
+      " of the previous numbers");
   int combinedNumber = 0;
   for (int i = 1; i <= 10; i++){
     print("The number is $i");
